764e971583709e06db4359da4d507129728c9573
Olufunmilola Babalola <osb1271.rit@gmail.com>
2018-02-10 14:16:17 -0500

    Initial Xcode project setup

---
+++ Wegmans Planner/Wegmans Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wegmans Planner
+//
+//  Created by Olufunmilola Babalola on 2/10/18.
+//  Copyright © 2018 TFW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
