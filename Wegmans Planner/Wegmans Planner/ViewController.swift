c25c74c1602dbe8ada45666f3b7b1771e950d7a6
Merry Ren <mxr6570@glados.cs.rit.edu>
2018-02-11 00:25:29 -0500

    Prices function API added and fixed UI with backend

--- Wegmans Planner/Wegmans Planner/ViewController.swift
+++ Wegmans Planner/Wegmans Planner/ViewController.swift
@@ -10,22 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //parseLocation()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    // PG: Removed for API Call Test
-    //override func didReceiveMemoryWarning() {
-    //    super.didReceiveMemoryWarning()
-    //    // Dispose of any resources that can be recreated.
-    //}
-    
     let location = "New York"
     let city = "Rochester"
     @IBOutlet var store_count: UILabel!
+    let price = 0.0
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //parseLocation()
+        parsePrices()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Reads location information from Wegmans API
     func parseLocation(){
         let url = URL(string: "https://api.wegmans.io/location/location/stores")!
         var request = URLRequest(url: url)
@@ -33,8 +34,6 @@ class ViewController: UIViewController {
         request.addValue("4982e31c064e40708e5984724133018a", forHTTPHeaderField: "Location-Subscription-Key")
         let task = URLSession.shared.dataTask(with: request){
             (data, response, error) in
-            //print(error)
-            //print(data)
             if (error != nil){
                 print(error)
                 return
@@ -53,10 +52,11 @@ class ViewController: UIViewController {
                                 }
                             }
                         }
+                        
                         DispatchQueue.main.async {
                             self.store_count.text = "\(close_store_arr.count)"
                         }
-                        print(close_store_arr)
+                        //print(close_store_arr)
                     }
                     else {
                         print(json)
@@ -69,12 +69,56 @@ class ViewController: UIViewController {
             }
         }
         task.resume()
-    }
+    }//end of parseLocation()
     
+    //Button for loading store counts in parseLocation()
     @IBAction func button_pressed(_ sender: Any) {
         parseLocation()
     }
-    
 
-}
+    
+    // Parses the Prices data from the Wegmans API
+    func parsePrices(){
+        let url = URL(string: "https://api.wegmans.io/price/cart/total")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("4982e31c064e40708e5984724133018a", forHTTPHeaderField: "Price-Subscription-Key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        var body = [String: Any]()
+        var product: [String : Int] = ["Sku" : 701819, "Quantity" : 1]
+        body["LineItems"] = [product]
+        body["StoreNumber"] = 25
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+        
+        // Prints the request to be sent to API
+        print(body)
+        
+        let task = URLSession.shared.dataTask(with: request){
+            (data, response, error) in
+            if (error != nil){
+                print(error)
+                return
+            }
+            if let data = data{
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                    if let dict_data = json as? [String : Any]{
+                        if let total = dict_data["Total"] as? Double{
+                            print(total)
+                        }
+                    }
+                    else {
+                        print(json)
+                        print("cast json to dictionary error")
+                    }
+                }
+                catch {
+                    print("no data")
+                }
+            }
+        }
+        task.resume()
+    }//end of parsePrices()
+    
+}// end of view controller
 
